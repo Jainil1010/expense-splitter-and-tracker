272178b019151dda1dbd0fccdcdadfbbca784ed7
Jainil1010 <jainilgohil1010@gmail.com>
2025-08-05 12:24:16 +0530

    added Remove member form group feature fixed adding of user which does not exists

--- server/controller/group.controller.js
+++ server/controller/group.controller.js
@@ -1,4 +1,5 @@
 import Group from "../models/group.model.js";
+import User from "../models/user.model.js";
 
 export const createGroup = async (req, res, next) => {
     try {
@@ -30,14 +31,36 @@ export const addMembers = async (req, res, next) => {
 
         if (!isAdmin) return res.status(403).json({ success: false, message: "Only admins can add members"});
 
+        const userToADd = await User.findById(userId);
+        if (!userToADd) return res.status(404).json({success: false, message: "User does not exists"});
+
         if (group.members.some(m => m.user.equals(userId)))
             return res.status(400).json({ success: false, message: "Member already exists in this grop" });
         
         group.members.push({ user: userId, role: "member" });
 
         await group.save();
-        
+
         res.json({ success: true, group });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const removeMember = async (req, res, next) => {
+    try {
+        const { groupId, userId } = req.params;
+        const group = await Group.findById(groupId);
+        if (!group) return res.status(404).json({ success: false, message: "Group not found" });
+
+        const isAdmin = group.members.some(m >= 
+            m.user.equals(req.user._id) && m.role === "admin" );
+
+        if (!isAdmin) return res.status(403).json({ success: false, message: "Only admins can remove members"});
+
+        group.members = group.members.filter(m => !m.user.equals(userId));
+        await group.save();
+        res.status(200).json({ success: true, group });
     } catch (error) {
         next(error);
     }
